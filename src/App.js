2de45ca194d9fddb2e73c892e0aa4dbb7a4f48ce
daniel-mawioo <mawioodaniel43@gmail.com>
2024-04-27 09:06:44 +0300

    temp component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import "./App.css";
+import hotBg from "./assets/hot.avif";
+import coldBg from "./assets/cold.png";
+import weatherIcon from "./assets/weather-app.png";
+import Descriptions from "./components/Descriptions";
 
 function App() {
   return (
-    <div className="App">
-      <h1> Weather App </h1>
+    <div className="app" style={{ backgroundImage: `url(${coldBg})` }}>
+      <div className="overlay">
+        <div className="container">
+          <div className="section section__inputs">
+            <input type="text" name="city" placeholder="Enter City..." />
+            <button>°C</button>
+          </div>
+          <div className="section section__temperature">
+            <div className="icon">
+              <h3>Nairobi, City</h3>
+              <img className="icon-img" src={weatherIcon} alt="weatherIcon" />
+              <h3>Cloudy</h3>
+            </div>
+            <div className="temperature">
+              <h1> 34 °C</h1>
+            </div>
+          </div>
+
+          {/* botton description */}
+          <Descriptions />
+        </div>
+      </div>
     </div>
   );
 }
